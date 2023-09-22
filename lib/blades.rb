14e93507ff541e28f259b73301f4d696ed07c81d
Mike Nicholaides <mike@nicholaides.com>
2023-09-22 10:40:27 -0400

    Working blades gem

--- lib/blades.rb
+++ lib/blades.rb
@@ -1,8 +1,32 @@
-# frozen_string_literal: true
-
 require_relative "blades/version"
 
+require "fileutils"
+
 module Blades
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.sh_to_pipe(*cmd)
+    stdout_r, stdout_w = IO.pipe
+    system(*cmd, exception: true, out: stdout_w)
+    stdout_w.close
+    stdout_r
+  end
+
+  def self.sh_to_file(*cmd, dst)
+    IO.copy_stream sh_to_pipe(*cmd), dst
+  end
+
+  def self.find_all = Dir["**/*.tmpl.*"].map { Template.new(_1) }
+
+  Template = Struct.new(:src) do
+    def dst = src.sub(/\.tmpl..+$/, "")
+
+    def ext?(str) = dst.end_with?(str)
+
+    def executable? = File.executable?(src)
+
+    def compile_to_dst = Blades.sh_to_file "./#{src}", dst
+
+    def clean
+      FileUtils.rm(dst) if File.exist?(dst)
+    end
+  end
 end
