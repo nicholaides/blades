2f207b168a1ddc9a070e9519c9b89f9200a85dc3
Mike Nicholaides <mike@nicholaides.com>
2023-11-14 10:05:01 -0500

    Add related_file helper

--- lib/blades.rb
+++ lib/blades.rb
@@ -17,6 +17,43 @@ module Blades
 
   def self.find_all = Dir["**/*.tmpl.*"].map { Template.new(_1) }
 
+  module Rel
+    module_function
+
+    def __caller_path__
+      Pathname.new(caller_locations.drop_while { _1.path == __FILE__ }.first.path)
+    end
+
+    def relative_file(path) = __caller_path__.parent + path
+
+    def related_file(ext) = related_file_to(__caller_path__, ext)
+
+    def related_file_to(base, ext)
+      possible_names = descend_exts(append_ext(base, ".X")).map { _1.sub_ext(ext) }
+
+      possible_names.detect { File.exist?(_1) } || raise(
+        "Couldn't find related #{ext.inspect} file to #{base.inspect}. Tried: #{possible_names.map { "- #{_1}" }}"
+      )
+    end
+
+    def append_ext(path, ext) = Pathname.new("#{path}#{ext}")
+
+    # e.g.: descend_exts("foo/bar.a.b.c").to_a => ["foo/bar.a.b.c", "foo/bar.a.b", "foo/bar.a", "foo/bar"]
+    def descend_exts(path)
+      return to_enum(:descend_exts, path) unless block_given?
+
+      current_path = path
+
+      loop do
+        yield current_path
+
+        break if current_path.extname == ""
+
+        current_path = current_path.sub_ext("")
+      end
+    end
+  end
+
   class Template
     attr_accessor :src
 
